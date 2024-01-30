ab244a084a24df66cc50f259a72f5b7e311a2534
sah <sajjad.heidarzadeh@adorsys.com>
2024-01-30 11:30:19 +0100

    create app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { TestComponent } from './test/test.component';
+import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
+
+const appRoutes: Routes = [
+  { path: '', component: RecipesComponent },
+  { path: 'sl', component: ShoppingListComponent },
+  { path: 'users', component: UsersComponent, children:[
+    { path: 'user/:id', component: UserComponent }
+  ] },
+  { path: 'recipe/:name', component: RecipeEditComponent },
+  { path: 'test/:id/edit', component: TestComponent },
+  {path: 'not-found', component: NotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Router, RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,17 +21,7 @@ import { UserComponent } from './users/user/user.component';
 import { UsersService } from './users/users.service';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const appRoutes: Routes = [
-  { path: '', component: RecipesComponent },
-  { path: 'sl', component: ShoppingListComponent },
-  { path: 'users', component: UsersComponent, children:[
-    { path: 'user/:id', component: UserComponent }
-  ] },
-  { path: 'recipe/:name', component: RecipeEditComponent },
-  { path: 'test/:id/edit', component: TestComponent },
-  {path: 'not-found', component: NotFoundComponent},
-  {path: '**', redirectTo: '/not-found'}
-];
+
 
 @NgModule({
   declarations: [
@@ -53,9 +42,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    AppRoutingModule
   ],
   providers: [RecipeService, ShoppingListService, UsersService],
   bootstrap: [AppComponent],
